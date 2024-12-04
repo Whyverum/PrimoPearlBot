088fd94d1dd0f3715a981edf8b73fd926b65a6bd
Whyverum <sergeyzavalin@outlook.com>
2024-12-05 05:57:47 +0700

    Файл подключения базы данных (в разработке)

---
+++ MySQL/db.py
@@ -0,0 +1,21 @@
+# BotCode/MySQL/db.py     (в разработке)
+
+import aiomysql
+
+
+# Функция подключение к базе данных     (в разработке)
+async def connect_db():
+    return await aiomysql.connect(host='your_host',
+                                  user='your_user',
+                                  password='your_password',
+                                  db='your_database',
+                                  charset='utf8mb4',
+                                  cursorclass=aiomysql.cursors.DictCursor)
+
+
+# Функция выполнения запросов к БД      (в разработке)
+async def execute_query(conn, query):
+    async with conn.cursor() as cur:
+        await cur.execute(query)
+        return await cur.fetchall()
+
