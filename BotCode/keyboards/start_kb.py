0b060346691d88c908c62828c47abc45770d30cd
Whyverum <sergeyzavalin@outlook.com>
2024-12-05 05:53:17 +0700

    Клавиатура для команды /start

---
+++ BotCode/keyboards/start_kb.py
@@ -0,0 +1,31 @@
+# start_kb.py
+
+from aiogram import Router
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+# Создание роутера "start_kb_router"
+router = Router(name="start_kb_router")
+
+
+# Класс с названиями кнопок
+class ButtonText:
+    Hello = "Привет!"
+    Help = "Помогите!"
+    Bye = "Пока-пока!"
+
+
+# Функция создания клавиатуры на команду: /start
+def get_start_kb() -> ReplyKeyboardBuilder:
+    button_hello = KeyboardButton(text=ButtonText.Hello)
+    button_help = KeyboardButton(text=ButtonText.Help)
+    button_bye = KeyboardButton(text=ButtonText.Bye)
+
+    buttons_first_row = [button_hello, button_help]
+    buttons_second_row = [button_bye]
+    markup = ReplyKeyboardMarkup(
+        keyboard=[buttons_first_row, buttons_second_row],
+        resize_keyboard=True,
+        one_time_keyboard=True,
+    )
+    return markup
