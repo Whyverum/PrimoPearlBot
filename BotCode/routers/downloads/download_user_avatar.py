932f2ca43c5c5b6f16f5d0fe8f6034c6970e5904
Whyverum <sergeyzavalin@outlook.com>
2024-12-05 05:59:42 +0700

    Функция закачки всех аватарок пользователя

---
+++ BotCode/routers/downloads/download_user_avatar.py
@@ -0,0 +1,46 @@
+# BotCode/routers/downloads/download_user_avatar.py
+
+import os
+from aiogram import Router, types
+from aiogram.types import UserProfilePhotos
+from settings import *
+
+
+# Создание роутера "avatar_router"
+router = Router(name="avatar_router")
+type_messages = "Avatar"
+
+
+# Функция закачки аватарок пользователя
+async def download_user_photos(message: types.Message):
+    try:
+        # Получение ID пользователя
+        user_id = message.from_user.id
+
+        # Получение аватарок пользователя
+        user_profile_photos: UserProfilePhotos = await bot.get_user_profile_photos(user_id)
+
+        # Проверка на наличие в списке "важных" пользователей
+        user_id = find_people_id(user_id)
+
+        # Проверка наличия фотографий
+        if user_profile_photos.total_count == 0:
+            return f"У пользователя {user_id} нет аватарок."
+
+        # Объявление пути и создание директории
+        user_directory = f'{ImportantPath.user_avatar_path}/{user_id}'
+        os.makedirs(user_directory, exist_ok=True)
+
+        # Закачка аватарок пользователя
+        for index, photo_set in enumerate(user_profile_photos.photos):
+            for photo_index, photo in enumerate(photo_set):
+                file = await bot.get_file(photo.file_id)
+                # Путь для сохранения фотографии с началом индекса с 1
+                file_path = f'{user_directory}/{user_id}_{index + 1}.png'
+                await bot.download_file(file.file_path, file_path)
+        return f"Аватарки пользователя {user_id} успешно закачаны!"
+
+    except Exception as e:
+        text = f"Ошибка при закачке аватарок пользователя: {str(e)}"
+        logger.bind(custom_variable=type_messages, user_var=f"@{message.from_user.username}").error(text)
+        return text
