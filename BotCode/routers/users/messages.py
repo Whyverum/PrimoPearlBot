4e0ccb8c8370d579c00e03c48627ce99d6fd0f6b
Whyverum <sergeyzavalin@outlook.com>
2024-12-05 06:02:49 +0700

    Обработчик всех сообщений

---
+++ BotCode/routers/users/messages.py
@@ -0,0 +1,76 @@
+# BotCode/routers/users/messages.py   (Разберись с предупреждениями и логгированием!!!!!)
+
+from settings import *
+from aiogram import Router, types, F
+from aiogram.types import ReplyKeyboardRemove
+
+from BotCode.keyboards.start_kb import ButtonText
+from ..downloads.download_user_avatar import download_user_photos
+
+# Создание роутера "common_message_router"
+router = Router(name="common_message_router")
+type_messages = "Messages"
+
+
+# Ответ бота на кнопку или сообщение: "Привет!"
+@router.message(F.text.lower() == ButtonText.Hello.lower())
+async def bye(message: types.Message):
+    type_messages = "Start_Button"
+    text_message = f"Привет, я бот. А ты кто?"
+    await message.reply(
+        text=text_message,
+    )
+    # logger.bind(custom_variable=type_messages, user_var=str(message.from_user.username)).info(
+    #     f"Получено сообщение из ({name}) : {message_type}")
+    return text_message
+
+
+# Ответ бота на кнопку или сообщение: "Помощь!"
+@router.message(F.text.lower() == ButtonText.Help.lower())
+async def help_message(message: types.Message):
+    type_messages = "Help_Button"
+    text_message = f"Привет, я надеюсь помогу тебе... Лучше напиши /help.."
+    await message.reply(
+        text=text_message,
+    )
+    return text_message
+
+
+# Ответ бота на кнопку или сообщение: "Пока-пока!"
+@router.message(F.text.lower() == ButtonText.Bye.lower())
+async def bye_message(message: types.Message):
+    text_message = f"Надеюсь скоро увидимся! Захочешь поговорить нажми на /start!"
+    await message.reply(
+        text=text_message,
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    return text_message
+
+
+# Ответ бота на сообщение: "Кошмар"
+@router.message(F.text.lower() == "кошмар")
+async def scary_message(message: types.Message):
+    text_message = f"Кошмар, тот еще!"
+    await message.reply(
+        text=text_message,
+    )
+    return text_message
+
+
+# Хэндлер на все сообщения и записывает данные
+@router.message()
+async def handle_all_messages(message: types.Message):
+    name = find_chat_id(message)
+    await logginger(message)
+    await download_user_photos(message)
+    message_type = types_message(message)
+
+    # Проверка на наличие текста и его типа
+    if message.text is None:
+        logger.bind(custom_variable=type_messages, user_var=str(message.from_user.username)).info(
+            f"Получено сообщение из ({name}) : {message_type}")
+    else:
+        logger.bind(custom_variable=type_messages, user_var=str(message.from_user.username)).info(
+            f"Получено сообщение из ({name}) : {message.text}")
+
+    return f"Получено новое сообщение!"
