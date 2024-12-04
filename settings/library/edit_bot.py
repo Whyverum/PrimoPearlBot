a9a270eba6c928dfc134201b101005ad52fbcbe8
Whyverum <sergeyzavalin@outlook.com>
2024-12-05 06:00:31 +0700

    Настройка автономных параметров бота

---
+++ settings/library/edit_bot.py
@@ -0,0 +1,83 @@
+# BotCode/settings/system/edit_bot.py
+
+from aiogram.types import ChatAdministratorRights
+from settings.configs.config import BotEdit
+from settings.library.bots import bot
+
+
+# Функция для выполнения всех настроек, если они не совпадают
+async def set_all():
+    await set_adm_rights()
+    await set_bot_name()
+    await set_bot_description()
+    await set_bot_short_description()
+    return f"Автономная настройка бота - завершена!"
+
+
+# Функция установки имени бота
+async def set_bot_name():
+    # Получаем текущее имя бота
+    current_name = (await bot.get_me()).first_name
+
+    # Проверяем, совпадает ли текущее имя с тем, которое мы хотим установить
+    if current_name != BotEdit.name:
+        await bot.set_my_name(BotEdit.name)
+        return f"Имя бота изменено на {BotEdit.name}!"
+    else:
+        return f"Имя бота уже установлено как {current_name}. Изменений не требуется."
+
+
+# Функция установки прав администратора
+async def set_adm_rights():
+    # Применить права администратора для бота
+    rights = ChatAdministratorRights(
+        is_anonymous=False,
+        can_manage_chat=True,
+        can_delete_messages=True,
+        can_manage_video_chats=True,
+        can_restrict_members=True,
+        can_promote_members=True,
+        can_change_info=True,
+        can_invite_users=True,
+        can_post_stories=True,
+        can_edit_stories=True,
+        can_delete_stories=True,
+        can_post_messages=True,
+        can_edit_messages=True,
+        can_pin_messages=True,
+        can_manage_topics=True,
+    )
+
+    # Применяем права только в случае изменения
+    current_rights = await bot.get_my_default_administrator_rights()
+    if current_rights != rights:
+        await bot.set_my_default_administrator_rights(rights)
+        return f"Админ права бота изменены!"
+    else:
+        return f"Админ права уже установлены и не требуют изменений."
+
+
+# Функция установки описания бота
+async def set_bot_description():
+    # Получаем текущее описание бота
+    current_description = await bot.get_my_description()
+
+    # Проверяем, совпадает ли текущее описание с тем, которое мы хотим установить
+    if current_description != BotEdit.description:
+        await bot.set_my_description(description=BotEdit.description)
+        return f"Описание бота изменено!"
+    else:
+        return f"Описание бота уже установлено и не требует изменений."
+
+
+# Функция установки короткого описания бота
+async def set_bot_short_description():
+    # Получаем текущее короткое описание бота
+    current_short_description = await bot.get_my_short_description()
+
+    # Проверяем, совпадает ли текущее короткое описание с тем, которое мы хотим установить
+    if current_short_description != BotEdit.short_description:
+        await bot.set_my_short_description(short_description=BotEdit.short_description)
+        return f"Короткое описание бота изменено!"
+    else:
+        return f"Короткое описание бота уже установлено и не требует изменений."
