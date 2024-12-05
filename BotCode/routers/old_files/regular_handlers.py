59613dc1b8670102949b70159987da616f08f903
Whyverum <sergeyzavalin@outlook.com>
2024-12-06 03:23:51 +0700

    Старый файл с регуляркой

--- BotCode/routers/old_files/regular_handlers.py
+++ BotCode/routers/old_files/regular_handlers.py
@@ -1,23 +1,29 @@
-# regular_handlers.py
+# BotCode/routers/old_files/regular_handlers.py
+# Регулярная функция, выдает тебе сообщение на код при сообщении с числами
 
 from aiogram import F, types, Router
 from magic_filter import RegexpMode
 from re import Match
 
-from settings.configs import config
-from settings import write_message_to_file
-from settings import write_user_info_to_file
+from BotSettings.configs import config
+from BotSettings import logginger
 
+# Настройка экспорта модулей и роутера
+__all__ = ("router",)
 router = Router(name="regular_handlers")
 
 
-# Хэндлер на цифирный код
+# Хэндлер на циферный код (регулярная функция)
 @router.message(
     F.from_user.id.in_(config.ListId.adm_list_id),
     F.text.regexp(r"(\d+)", mode=RegexpMode.MATCH).as_("code"),
 )
 async def handle_code(message: types.Message, code: Match[str]):
-    await message.reply(f"Your code: {code.group()}")
-    await write_user_info_to_file(message.from_user)
-    await write_message_to_file(message)
-    return f"Цифирный код"
+    # Вывод сообщения
+    text = f"Ваш код: {code.group()}"
+    await message.reply(text)
+
+    # Включение логирования в файл
+    await logginger(message)
+
+    return text
