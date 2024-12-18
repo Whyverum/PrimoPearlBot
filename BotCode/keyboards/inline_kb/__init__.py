3c350c32466bff465e10bc9fe8335fee169d4877
Whyverum <sergeyzavalin@outlook.com>
2024-12-18 12:16:23 +0700

    Инициализация модулей

--- BotCode/keyboards/inline_kb/__init__.py
+++ BotCode/keyboards/inline_kb/__init__.py
@@ -2,18 +2,13 @@
 # Инициализация пакета keyboards, для работы с клавиатурами
 
 from aiogram import Router
-from .inline_kb import router as inline_kb_router
-from .reply_kb import router as reply_kb_router
-from .inline_kb import *
-from .reply_kb import *
+from .actor_kb import *
+from .randnum_kb import *
 
 # Объявление роутера и настройка экспорта модулей
-__all__ = ("router", "inline_kb", "reply_kb",)
-router = Router(name="kb_router")
+__all__ = ("router", "actor_kb", "ButtonInl")
+router = Router(name="inline_kb_router")
 
 
 # Список подключаемых роутеров сверху-вниз
-router.include_routers(
-    inline_kb_router,
-    reply_kb_router,
-)
+# router.include_routers()
--- BotCode/routers/common/__init__.py
+++ BotCode/routers/common/__init__.py
@@ -3,6 +3,7 @@
 
 from aiogram import Router
 from .messages import router as common_message_router
+from .phrase import router as phrase_message_router
 
 
 # Объявление роутера и настройка экспорта модулей
@@ -12,8 +13,8 @@ router = Router(name="users_head_router")
 
 # Список подключаемых роутеров сверху-вниз
 router.include_routers(
-    common_message_router,
+    phrase_message_router,
 )
 
 # Идет самым последним, если другие роутеры не сработали
-# router.include_router(common_message_router)
+router.include_router(common_message_router)
