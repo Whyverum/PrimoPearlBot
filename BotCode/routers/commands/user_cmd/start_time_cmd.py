e99e529cd3458366535fd7886aa933cd7536f5db
Whyverum <sergeyzavalin@outlook.com>
2024-12-18 07:25:10 +0700

    Команда на отправку времени запуска пользователю

---
+++ BotCode/routers/commands/user_cmd/start_time_cmd.py
@@ -0,0 +1,38 @@
+# BotCode/routers/commands/admin_cmd/start_time_cmd.py
+# Работа с командой /start_time, для вывода времени запуска бота
+
+from aiogram import Router, types, F
+from aiogram.filters import Command
+from BotLibrary import *
+
+# Создание роутера и настройка экспорта модулей
+__all__ = ("router", "start_time", "log_type",)
+router = Router(name="start_time_router")
+log_type = "Start_Time"
+
+# Список ключевых слов для команды
+start_time_keywords = ["start_time", "stime", "старт_время", "время_старта", "с_время",
+                       "ыефке_ешьу", "ыешьу", "cnfhn_dhtvcz", "dhtvz_cnfhnf", "c_dhtvz", 
+                       "бот_время", "время_запуска", "бот_врем"]
+
+
+# Хэндлер на команду /start_time
+@router.message(Command(*start_time_keywords, prefix=BotEdit.prefixs, ignore_case=True))
+@router.message(F.text.lower().in_(start_time_keywords))
+async def start_time(message: types.Message, started_at: str, started_at_msk: str):
+    try:
+        # Вывод сообщения пользователю
+        text = f"использовал(а) команду /{log_type.lower()}"
+        await message.answer(f"Бот @{BotInfo.username} запущен: "
+                             f"\nХост: <b>{started_at}</b>"
+                             f"\nМСК: <b>{started_at_msk}</b>")
+
+        # Активация логгера
+        await cmd_logginger(message, log_type, text)
+        return text
+
+    # Проверка на ошибку и ее логирование
+    except Exception as e:
+        error_text = await error_cmd_logginger(message, log_type, e)
+        return error_text
+
