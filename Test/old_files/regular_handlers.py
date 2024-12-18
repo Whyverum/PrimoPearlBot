ec5dc7e9f71465bc23dd81af75aae5738209b44c
Whyverum <sergeyzavalin@outlook.com>
2024-12-18 07:15:22 +0700

    Регулярные сообщения (старые файлы)

---
+++ Test/old_files/regular_handlers.py
@@ -0,0 +1,29 @@
+# BotCode/routers/old_files/regular_handlers.py
+# Регулярная функция, выдает тебе сообщение на код при сообщении с числами
+
+from aiogram import F, types, Router
+from magic_filter import RegexpMode
+from re import Match
+
+from BotLibrary import logginger
+import config
+
+# Настройка экспорта модулей и роутера
+__all__ = ("router",)
+router = Router(name="regular_handlers")
+
+
+# Хэндлер на циферный код (регулярная функция)
+@router.message(
+    F.from_user.id.in_(config.ListId.adm_list_id),
+    F.text.regexp(r"(\d+)", mode=RegexpMode.MATCH).as_("code"),
+)
+async def handle_code(message: types.Message, code: Match[str]):
+    # Вывод сообщения
+    text = f"Ваш код: {code.group()}"
+    await message.reply(text)
+
+    # Включение логирования в файл
+    await logginger(message)
+
+    return text
