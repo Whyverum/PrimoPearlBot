5762a5d6ad59870d37bc60cf1df81f48f178ef7b
Whyverum <sergeyzavalin@outlook.com>
2024-12-18 12:18:16 +0700

    Новая команда /randnum редактирует сообщение и число

---
+++ BotCode/routers/callback_handlers/randnum_kb_cb.py
@@ -0,0 +1,26 @@
+from random import randint
+
+from aiogram import Router, F
+from aiogram.types import CallbackQuery
+from keyboards.inline_kb.randnum_kb import ButtonInl, get_randnum_kb
+
+router = Router(name="randnum_kb_cb_router")
+
+@router.callback_query(F.data == ButtonInl.mark_cbd)
+async def random_site_cb(callback_query: CallbackQuery):
+    await callback_query.answer()
+
+    # Новый текст и клавиатура
+    new_text = f"Какая оценка у тебя будет сегодня: {randint(1, 5)}"
+    new_reply_markup = get_randnum_kb("Получить ответ от Таро")
+
+    # Текущий текст и клавиатура
+    current_text = callback_query.message.text
+    current_reply_markup = callback_query.message.reply_markup
+
+    # Проверяем, отличаются ли текст и клавиатура
+    if current_text != new_text or current_reply_markup != new_reply_markup:
+        await callback_query.message.edit_text(
+            text=new_text,
+            reply_markup=new_reply_markup,
+        )
