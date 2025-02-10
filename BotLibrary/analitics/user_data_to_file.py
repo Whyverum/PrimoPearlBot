a4a86853fb1a761b6faefb128b996b2b02bdcde6
sergey <sergeyzavalin@outlook.com>
2025-02-11 01:12:30 +0700

    Исправлен баг с импортом модуля времени

--- BotLibrary/analitics/user_data_to_file.py
+++ BotLibrary/analitics/user_data_to_file.py
@@ -7,7 +7,7 @@ import datetime
 
 from aiogram.types import User, Birthdate
 from ..configs import *
-from ..library.time import TimeVariable
+from BotLibrary.timers.time import TimeVariable
 
 # Настройка экспорта модулей и логирования
 __all__ = ("write_user_info_to_file", "format_user_record",)
